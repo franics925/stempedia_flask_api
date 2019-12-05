1b562abf9947bff5c40cef809b8d0fa884f4050f
Michael <michaelhofmann987@gmail.com>
2019-12-05 00:04:21 -0800

    Database change

--- app.py
+++ app.py
@@ -1,55 +1,31 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from flask_login import LoginManager, UserMixin, current_user, login_user
-from werkzeug.security import generate_password_hash, check_password_hash
-# from datetime import datetime
-
+from flask_marshmallow import Marshmallow 
+from datetime import datetime
 import os
 
 # Init app
 app = Flask(__name__)
-login = LoginManager(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/stem'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Init db
 db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
-# Configure app settings with migrations
-migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
-# CLASSES
-class User(UserMixin, db.Model):
+class User(db.Model):
 
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100))
   email = db.Column(db.String(100))
   description = db.Column(db.String(250))
-  password_hash = db.Column(db.String(128))
 
-  def __init__(self, id_, name, email, description):
-    self.id = id_
+  def __init__(self, name, email, description):
     self.name = name
     self.email = email
     self.description = description
-
-  def set_password(self, password):
-    self.password_hash = generate_password_hash(password)
-
-  def check_password(self, password):
-    return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
 
@@ -61,7 +37,7 @@ class Post(db.Model):
   body = db.Column(db.String(5000))
   tags = db.Column(db.String(200))
   upvote = db.Column(db.Integer)
-  registered = db.Column(db.DateTime, nullable = False)
+  registered = db.Column(db.DateTime, default=datetime.utcnow)
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
   def __init__(self, name, description, category, topic, body, tags):
@@ -85,7 +61,7 @@ class Comment(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(150))
   body = db.Column(db.String)
-  created = db.Column(db.DateTime, nullable = False)
+  created = db.Column(db.DateTime, default=datetime.utcnow)
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
   post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
   upvote = db.Column(db.Integer)
@@ -111,34 +87,118 @@ class CategorySchema(ma.Schema):
         fields = ('id', 'name')
 
 class CommentSchema(ma.Schema):
-	class meta:
-		fields = ('title', 'body', 'upvote', 'downvote')
+    class Meta:
+        fields = ('id', 'title', 'body', 'upvote', 'downvote')
 
 # INIT SCHEMA
 user_schema = UserSchema()
 users_schema = PostSchema(many=True)
 post_schema = PostSchema()
 posts_schema = PostSchema(many=True)
+comment_schema = CommentSchema()
+comments_schema = CommentSchema(many=True)
 category_schema = CategorySchema()
 categories_schema = CategorySchema(many=True)
 
-
-# ROUTES
+# Create a user
 @app.route('/user', methods=['POST'])
 def add_user():
-    name = request.get_json('name')
-    email = request.get_json('email')
-    description = request.get_json('description')
+  name = request.json['name']
+  email = request.json['email']
+  description = request.json['description']
 
-    new_user = User(name, email, description)
+  new_user = User(name, email, description)
 
-    # add to db
-    db.session.add(new_user)
-    db.session.commit()
+  db.session.add(new_user)
+  db.session.commit()
 
-    return user_schema.jsonify(new_user)
+  return user_schema.jsonify(new_user)
 
+@app.route('/post', methods=['POST'])
+def add_post():
+  name = request.json['name']
+  description = request.json['description']
+  category = request.json['category']
+  topic = request.json['topic']
+  body = request.json['body']
+  tags = request.json['tags']
+
+  new_post = Post(name, description, category, topic, body, tags)
+
+  db.session.add(new_post)
+  db.session.commit()
+
+  return post_schema.jsonify(new_post)
+
+@app.route('/category', methods=['POST'])
+def add_category():
+  name = request.json['name']
+  
+  new_category = Category(name)
+
+  db.session.add(new_category)
+  db.session.commit()
+
+  return category_schema.jsonify(new_category)
+
+@app.route('/comment', methods=['POST'])
+def add_comment():
+  title = request.json['title']
+  body = request.json['body']
+  upvote = request.json['upvote']
+  downvote = request.json['downvote']
+
+  new_comment = Comment(title, body, upvote, downvote)
+
+  db.session.add(new_comment)
+  db.session.commit()
+
+  return comment_schema.jsonify(new_comment)
+
+@app.route('/post', methods=['GET'])
+def get_posts():
+  all_posts = Post.query.all()
+  result_posts = posts_schema.dump(all_posts)
+  return jsonify(result_posts)
+
+@app.route('/comment', methods=['GET'])
+def get_comments():
+  all_comments = Comment.query.all()
+  result_comments = comments_schema.dump(all_comments)
+  return jsonify(result_comments)
+
+@app.route('/post/<id>', methods=['GET'])
+def get_post(id):
+  post = Post.query.get(id)
+  return post_schema.jsonify(post)
+
+@app.route('/user/<id>', methods=['GET'])
+def get_user(id):
+  user = User.query.get(id)
+  return user_schema.jsonify(user)
+
+@app.route('/post/<id>', methods=['PUT'])
+def update_post(id):
+  post = Post.query.get(id)
+
+  name = request.json['name']
+  description = request.json['description']
+  category = request.json['category']
+  topic = request.json['topic']
+  body = request.json['body']
+  tags = request.json['tags']
+
+  post.name = name
+  post.description = description
+  post.category = category
+  post.topic = topic
+  post.body = body
+  post.tags = tags
+
+  db.session.commit()
+
+  return post_schema.jsonify(post)
 
 # Run Server
 if __name__ == '__main__':
-    manager.run()
+  app.run(debug=True)
